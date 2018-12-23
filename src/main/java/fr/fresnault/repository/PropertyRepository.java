699e86442bb28e759cc786f76f0c1894a6309f39
François Esnault <francois.esnault53@gmail.com>
2018-12-23 18:13:04 +0100

    Récupération de tous les biens immobiliers en France métropolitaine disponibles sur Leboncoin (~950K)

--- src/main/java/fr/fresnault/repository/PropertyRepository.java
+++ src/main/java/fr/fresnault/repository/PropertyRepository.java
@@ -1,16 +1,17 @@
 package fr.fresnault.repository;
 
-import fr.fresnault.domain.Property;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import fr.fresnault.domain.Property;
 
 
 /**
  * Spring Data MongoDB repository for the Property entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface PropertyRepository extends MongoRepository<Property, String> {
+
+	boolean existsByReference(Integer reference);
 
 }
--- src/main/java/fr/fresnault/service/LeBonCoinScrapperService.java
+++ src/main/java/fr/fresnault/service/LeBonCoinScrapperService.java
@@ -35,55 +35,80 @@ public class LeBonCoinScrapperService {
 	public void runScrap() {
 		RestTemplate restTemplate = new RestTemplate();
 
-		// url
 		String leboncoinResourceUrl = "https://api.leboncoin.fr/finder/search";
 
-		// headers
 		HttpHeaders headers = new HttpHeaders();
 		headers.put("api_key", Arrays.asList("ba0c2dad52b3ec"));
 
-		int pageIndex = 0;
-		boolean hasResults = true;
+		// metropolitan departments
+		for (int department = 1; department <= 95; department++) {
+			int pageIndex = 0;
+			int nbRetry = 0;
+			boolean hasResults = true;
 
-		while (hasResults) {
-			log.info("Récupération de la page {}", pageIndex);
+			log.info("Department number {}", department);
+			while (hasResults && nbRetry < 10) {
+				log.info("Page number {}", pageIndex);
 
-			// body
-			int offset = pageIndex * 100;
-			String body = "{\"filters\":{\"category\":{\"id\":\"9\"},\"enums\":{\"ad_type\":[\"offer\"],\"real_estate_type\":[\"1\",\"2\",\"3\"]},\"keywords\":{},\"location\":{},\"ranges\":{}},\"limit\":100, \"offset\": "
-					+ offset + ", \"limit_alu\":0}";
-			HttpEntity<String> request = new HttpEntity<>(body, headers);
+				// body
+				int offset = pageIndex * 100;
+				String body = generateBodyRequest(department, offset);
+				HttpEntity<String> request = new HttpEntity<>(body, headers);
 
-			// post request
-			ResponseEntity<LeBonCoinPageResults> response = restTemplate.postForEntity(leboncoinResourceUrl, request,
-					LeBonCoinPageResults.class);
+				// post request
+				ResponseEntity<LeBonCoinPageResults> response = restTemplate.postForEntity(leboncoinResourceUrl,
+						request, LeBonCoinPageResults.class);
 
-			if (response.getStatusCode().equals(HttpStatus.OK)) {
-				for (LeBonCoinProperty property : response.getBody().getProperties()) {
-					Property propertyToSave = new Property().reference(property.getReference())
-							.publicationDate(property.getPublicationDate().atZone(ZoneId.systemDefault()))
-							.categoryName(property.getCategoryName()).subject(property.getSubject())
-							.body(property.getBody()).url(property.getUrl()).price(property.getPriceValue())
-							.attributes(property.getAttributes()).location(property.getLocation());
+				if (response.getStatusCode().equals(HttpStatus.OK) && response.getBody().getProperties() != null) {
+					if (nbRetry > 0) {
+						nbRetry = 0;
+					}
+					for (LeBonCoinProperty property : response.getBody().getProperties()) {
+						Integer reference = property.getReference();
+						if (!propertyRepository.existsByReference(reference)) {
+							Property propertyToSave = new Property().reference(reference)
+									.publicationDate(property.getPublicationDate().atZone(ZoneId.systemDefault()))
+									.categoryName(property.getCategoryName()).subject(property.getSubject())
+									.body(property.getBody()).url(property.getUrl()).price(property.getPriceValue())
+									.attributes(property.getAttributes()).location(property.getLocation());
+							propertyRepository.save(propertyToSave);
+							log.info("Property saved #{}", reference);
+						} else {
+							log.info("Property already exists #{}", reference);
+						}
+					}
 
-					propertyRepository.save(propertyToSave);
-				}
+					if (response.getBody().getProperties().size() < 100) {
+						hasResults = false;
+					}
 
-				if (response.getBody().getTotal() < offset) {
-					hasResults = false;
-				}
+					pageIndex++;
+				} else {
+					// If an error occurred during the request, we wait some
+					// seconds before recall the API
+					log.info("Status code : {}", response.getStatusCode());
+					if (pageIndex < 300) {
+						try {
+							Thread.sleep(1000 * RandomUtils.nextInt(20));
+						} catch (InterruptedException e) {
+							hasResults = false;
+						}
+					} else {
+						hasResults = false;
+					}
 
-				pageIndex++;
-			} else {
-				log.info("Status code : {}", response.getStatusCode());
-				try {
-					Thread.sleep(1000 * RandomUtils.nextInt(20));
-				} catch (InterruptedException e) {
-					hasResults = false;
+					nbRetry++;
 				}
 			}
+
 		}
 
+	}
+
+	private String generateBodyRequest(int department, int offset) {
+		return "{\"filters\":{\"category\":{\"id\":\"8\"},\"enums\":{\"ad_type\":[\"offer\"]},\"keywords\":{},\"location\":{\"departments\":[\""
+				+ department + "\"]},\"ranges\":{}},\"limit\":100, \"offset\": "
+				+ offset + ", \"limit_alu\":0}";
 	}
 
 }
